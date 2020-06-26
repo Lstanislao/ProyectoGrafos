ba533c04227ddb507741171e623447f6bcbcf4ea
sosag <sosag@192.168.5.102>
2020-06-25 20:30:40 -0400

    Funcion txt - Guille (25/06)

--- ProyectoGrafos/src/Grafo/Ori.java
+++ ProyectoGrafos/src/Grafo/Ori.java
@@ -5,11 +5,24 @@
  */
 package Grafo;
 
+import Ventanas.interfazRegistro;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+
 /**
  *
  * @author orian
  */
 public class Ori {
+
+    File miArchivo;
+    FileReader leer;
+    BufferedReader almacenamiento;
+    String cadena, texto = "";
+
+
 
     public static String EncontrarCaminos(Grafo g) {
         String caminos = "caminos\n";
@@ -25,4 +38,29 @@ public class Ori {
         return caminos;
     }
 
+    public void copiarTxt(File archivoExterno, File archivoFijo) {
+        try {
+            leer = new FileReader(archivoExterno);
+            almacenamiento = new BufferedReader(leer);
+
+            try {
+                while ((cadena = almacenamiento.readLine()) != null) {
+                    texto += cadena + "\n";
+                }
+                PrintWriter escribir;
+                archivoFijo.createNewFile();
+                escribir = new PrintWriter(archivoFijo,"utf-8");
+                
+                escribir.println(texto);
+                escribir.close();
+
+            } catch (IOException ex) {
+                Logger.getLogger(Ori.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Ori.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
+
+
